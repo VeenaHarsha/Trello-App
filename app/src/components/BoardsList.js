3704c5055f6461027aa9eca5e1410ec293bef31b
VeenaHarsha <veena.achyuth@gmail.com>
2020-05-22 18:32:31 +0530

    BoardsList

---
+++ app/src/components/BoardsList.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import BoardItem from './BoardItem'
+
+function BoardsList ({ boards }) {
+  return boards.map(board => <BoardItem key={board.id} board={board} />)
+}
+
+export default BoardsList
