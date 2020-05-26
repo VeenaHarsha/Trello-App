9b79e06596122768395f74f7e491052c67481a56
VeenaHarsha <veena.achyuth@gmail.com>
2020-05-26 23:08:25 +0530

    Drag n Drop changes added

--- app/src/components/Cards/CardItem.js
+++ app/src/components/Cards/CardItem.js
@@ -1,9 +1,18 @@
 import React from 'react'
 
 function CardItem ({ card }) {
+  const dragStartHandler = (e, id) => {
+    e.dataTransfer.setData('id', id)
+  }
+
   return (
     <>
-      <div key={card.id} className='card-div'>
+      <div
+        key={card.id}
+        className='card-div'
+        draggable
+        onDragStart={(e) => dragStartHandler(e, card.id)}
+      >
         <p>{card.card_desc}</p>
       </div>
     </>
